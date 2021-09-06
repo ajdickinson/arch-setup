19101a0632a88ceb5ace6386eec4d9fc13e45c28
ajdickinson <adickin3@uoregon.edu>
2021-09-06 16:59:37 -0700

    git setup bash script updated

---
+++ install-00-git_setup.sh
@@ -0,0 +1,24 @@
+# configure git
+git config --global user.name "ajdickinson"
+git config --global user.email "adickin3@uoregon.edu"
+
+# generate a new ssh key
+ssh-keygen -t ed25519 -C "adickin3@uoregon.edu"
+
+# follow instructions to set new ssh directory + passcode
+
+# now add ssh key to ssh agent
+eval "$(ssh-agent -s)"
+ssh-add ~/.shh/id_ed25519
+
+# install xclip to copy contents of ssh file
+# trizen -S xclip
+xclip -selection clipboard < ~/.ssh/id_ed25519.pub
+
+# instructions for adding key to github account on browser
+echo go to settings
+echo click ssh and gpg keys
+echo click new ssh key or add ssh key
+echo paste key into key field
+echo click add ssh key
+
