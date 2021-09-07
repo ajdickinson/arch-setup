d5d5fc5e04a4f23944fbb80960deeeb0de7056d5
ajdickinson <adickin3@uoregon.edu>
2021-09-07 10:48:52 -0700

    git setup bash script updatedf

--- install-00-git_setup.sh
+++ install-00-git_setup.sh
@@ -12,7 +12,7 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.shh/id_ed25519
 
 # install xclip to copy contents of ssh file
-# trizen -S xclip
+# sudo pacman -S xclip
 xclip -selection clipboard < ~/.ssh/id_ed25519.pub
 
 # instructions for adding key to github account on browser
@@ -21,4 +21,16 @@ echo click ssh and gpg keys
 echo click new ssh key or add ssh key
 echo paste key into key field
 echo click add ssh key
+
+# clone arch-setup repo
+# install github command line interface
+sudo pacman -S github-cli
+
+# authenticate after install
+gh auth login
+
+# follow instruction to link to github account in browser
+
+# clone arch-setup repo
+gh repo clone arch-setup
 
